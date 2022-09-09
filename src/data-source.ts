5b465aaae6288bd584cb782548631eb71ceeb348
SusimaraRoberti <susimararoberti@outlook.com>
2022-09-09 17:00:26 -0300

    feat:subindo banco

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,8 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
+require("dotenv").config();
+
 const AppDataSource = new DataSource(
   process.env.NODE_ENV === "test"
     ? {
@@ -11,15 +13,21 @@ const AppDataSource = new DataSource(
       }
     : {
         type: "postgres",
-        host: process.env.DB_HOST,
-        port: 5432,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB,
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
         logging: true,
-        synchronize: true,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
+        synchronize: false,
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/src/entities/*.js"]
+            : ["src/entities/*.ts"],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/src/migrations/*.js"]
+            : ["src/migrations/*.ts"],
       }
 );
 
